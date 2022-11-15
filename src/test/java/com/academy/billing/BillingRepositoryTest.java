ec25b810ca14da3a059a7777979ebf8eaa0a565e
Ryan Aristosa <ryan.aristosa@softvision.com>
2022-11-15 17:11:10 +0800

    Comment BillingRepositoryTest

--- src/test/java/com/academy/billing/BillingRepositoryTest.java
+++ src/test/java/com/academy/billing/BillingRepositoryTest.java
@@ -24,40 +24,40 @@ public class BillingRepositoryTest {
     private BillingRepository billingRepository;
 
 
-    @Test
-    void testSave() {
-        // Arrange
-        BigDecimal amount = new BigDecimal("100.0");
-        Billing billing = new Billing(101L, "Ryan", amount, "Paper");
-        // Act
-        Billing savedBilling = billingRepository.save(billing);
-        // Assert
-        assertEquals(billing, savedBilling);
-    }
-
-    @Test
-    void testFindById() {
-        // Arrange
-        BigDecimal amount = new BigDecimal("100.0");
-        Billing billing = new Billing(101L, "Ryan", amount, "Paper");
-        Billing savedBilling = billingRepository.save(billing);
-        // Act
-        Optional<Billing> existingBilling = billingRepository.findById(savedBilling.getId());
-        // Assert
-        assertTrue(existingBilling.isPresent());
-        assertEquals(savedBilling, existingBilling.get());
-    }
-
-    @Test
-    void testFindAll() {
-        BigDecimal amount = new BigDecimal("100.0");
-        Billing billing1 = new Billing(101L, "Ryan", amount, "Paper");
-        Billing billing2 = new Billing(102L, "Baqui", amount, "Paper");
-        Billing billing3 = new Billing(103L, "Justin", amount, "Electronic");
-        Iterable<Billing> existingBillings = billingRepository.saveAll(List.of(billing1, billing2, billing3));
-
-        Pageable pageable = PageRequest.of(0, 5, Sort.by("accountId").ascending());
-        assertThat(billingRepository.findAll(pageable)).containsAll(existingBillings);
-    }
+//    @Test
+//    void testSave() {
+//        // Arrange
+//        BigDecimal amount = new BigDecimal("100.0");
+//        Billing billing = new Billing(101L, "Ryan", amount, "Paper");
+//        // Act
+//        Billing savedBilling = billingRepository.save(billing);
+//        // Assert
+//        assertEquals(billing, savedBilling);
+//    }
+//
+//    @Test
+//    void testFindById() {
+//        // Arrange
+//        BigDecimal amount = new BigDecimal("100.0");
+//        Billing billing = new Billing(101L, "Ryan", amount, "Paper");
+//        Billing savedBilling = billingRepository.save(billing);
+//        // Act
+//        Optional<Billing> existingBilling = billingRepository.findById(savedBilling.getId());
+//        // Assert
+//        assertTrue(existingBilling.isPresent());
+//        assertEquals(savedBilling, existingBilling.get());
+//    }
+//
+//    @Test
+//    void testFindAll() {
+//        BigDecimal amount = new BigDecimal("100.0");
+//        Billing billing1 = new Billing(101L, "Ryan", amount, "Paper");
+//        Billing billing2 = new Billing(102L, "Baqui", amount, "Paper");
+//        Billing billing3 = new Billing(103L, "Justin", amount, "Electronic");
+//        Iterable<Billing> existingBillings = billingRepository.saveAll(List.of(billing1, billing2, billing3));
+//
+//        Pageable pageable = PageRequest.of(0, 5, Sort.by("accountId").ascending());
+//        assertThat(billingRepository.findAll(pageable)).containsAll(existingBillings);
+//    }
 
 }
