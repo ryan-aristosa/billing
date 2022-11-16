52e94fdbabc7b94f1165835567d312fef7548e41
Ryan Aristosa <ryan.aristosa@softvision.com>
2022-11-16 16:24:09 +0800

    Add user type

--- src/main/java/com/academy/billing/model/UserEntity.java
+++ src/main/java/com/academy/billing/model/UserEntity.java
@@ -1,43 +1,22 @@
 package com.academy.billing.model;
 
+import com.academy.billing.enums.UserType;
+import lombok.Data;
+
 import javax.persistence.*;
 
 @Entity
+@Data
 public class UserEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column
     private String username;
-
-    @Column
     private String password;
 
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    @Enumerated(value = EnumType.STRING)
+    private UserType type;
 
 }
