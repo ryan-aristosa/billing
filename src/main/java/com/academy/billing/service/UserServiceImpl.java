6b7d579f79f72c08cd0247f6db092f565e380a56
Ryan Aristosa <ryan.aristosa@softvision.com>
2022-11-16 16:19:57 +0800

    Add oAuth2

---
+++ src/main/java/com/academy/billing/service/UserServiceImpl.java
@@ -0,0 +1,35 @@
+package com.academy.billing.service;
+
+import com.academy.billing.model.UserEntity;
+import com.academy.billing.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("Invalid user");
+        }
+        return new User(user.getUsername(), user.getPassword(), getAuthority());
+    }
+
+    private List<SimpleGrantedAuthority> getAuthority() {
+        return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"));
+    }
+
+}
