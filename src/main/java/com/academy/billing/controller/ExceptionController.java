d5fb31eceb6bac9b49026133ed68b60d446d9d7f
Ryan Aristosa <ryan.aristosa@softvision.com>
2022-11-17 18:33:42 +0800

    Separate billing and user exception

--- src/main/java/com/academy/billing/controller/ExceptionController.java
+++ src/main/java/com/academy/billing/controller/ExceptionController.java
@@ -1,6 +1,7 @@
 package com.academy.billing.controller;
 
-import com.academy.billing.exception.RecordNotFoundException;
+import com.academy.billing.exception.BillingNotFoundException;
+import com.academy.billing.exception.UserNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -9,9 +10,14 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class ExceptionController {
 
-	@ExceptionHandler(value = RecordNotFoundException.class)
-	public ResponseEntity<Object> exception() {
-		return new ResponseEntity<>("Record not found", HttpStatus.NOT_FOUND);
+	@ExceptionHandler(value = BillingNotFoundException.class)
+	public ResponseEntity<Object> billingNotFound() {
+		return new ResponseEntity<>("Billing not found", HttpStatus.NOT_FOUND);
+	}
+
+	@ExceptionHandler(value = UserNotFoundException.class)
+	public ResponseEntity<Object> recordNotFound() {
+		return new ResponseEntity<>("User not found", HttpStatus.NOT_FOUND);
 	}
 
 }
