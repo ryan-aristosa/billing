f32afd4ea586422c1358378eacd3729864589806
Ryan Aristosa <ryan.aristosa@softvision.com>
2022-11-18 10:24:37 +0800

    Handle bad request and not found

--- src/main/java/com/academy/billing/controller/ExceptionController.java
+++ src/main/java/com/academy/billing/controller/ExceptionController.java
@@ -6,6 +6,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @ControllerAdvice
 public class ExceptionController {
@@ -14,6 +16,20 @@ public class ExceptionController {
 	public ResponseEntity<Object> handleRecordNotFound(Exception e) {
 		String message = e.getMessage() + " not found";
 		return new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
+	}
+
+	// 400 Bad Request
+	// Ex. expected Long as path variable, but found String
+	@ExceptionHandler(value = MethodArgumentTypeMismatchException.class)
+	public ResponseEntity<Object> handleBadRequest() {
+		return new ResponseEntity<>("Bad request", HttpStatus.BAD_REQUEST);
+	}
+
+	// 404 Not Found
+	// Ex. wrong url
+	@ExceptionHandler(value = NoHandlerFoundException.class)
+	public ResponseEntity<Object> handleNotFound() {
+		return new ResponseEntity<>("Not found", HttpStatus.NOT_FOUND);
 	}
 
 }
